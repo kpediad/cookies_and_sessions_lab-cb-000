8b6f0b9961bb80707fcfcb793b8e291d625c506b
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 09:31:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :cart
 
   def cart
-    session[:cart] = [] unless session[:cart]
-    session[:cart]
+    session[:cart] ||= []
   end
 
 end
