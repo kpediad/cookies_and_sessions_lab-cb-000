a1b46f499acfb2f63c052ef9255d7f229c44de32
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 08:40:33 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ActionController::Base
   def index
+    @cart = cart
   end
 
   def add
+    @item = Item.find(params[:id])
+    cart << @item.id
+    redirect_to root_path 
   end
 end
